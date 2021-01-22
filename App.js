77b52b2ead7c44a35a4052b76fe0f29b4ad89f8a
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-22 16:12:54 -0500

    feat(add dummy data markers to map)

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
+const dummyData = [
+  {
+    latitude: 40.679728,
+    longitude: -74.014908,
+  },
+  {
+    latitude: 40.674812,
+    longitude: -74.016092,
+  },
+  {
+    latitude: 40.678005,
+    longitude: -74.010903,
+  },
+];
+
 export default function App() {
   const [error, setError] = useState(null);
-  const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 51.5079145,
+    longitude: -0.0899163,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
 
   useEffect(() => {
     (async () => {
@@ -16,9 +36,9 @@ export default function App() {
         setError('Permission to access location was denied');
         return;
       }
-
       let location = await Location.getCurrentPositionAsync();
-      setLocation(location);
+      setRegion(location);
+      console.log('REGION', region);
     })();
   }, []);
 
@@ -26,8 +46,18 @@ export default function App() {
     <View style={styles.container}>
       <MapView
         style={StyleSheet.absoluteFillObject}
-        provider={MapView.PROVIDER_GOOGLE}
-      ></MapView>
+        initialRegion={{
+          latitude: 40.6734,
+          longitude: -74.0083,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
+        <Marker coordinate={dummyData[0]} />
+        <Marker coordinate={dummyData[1]} />
+        <Marker coordinate={dummyData[2]} />
+      </MapView>
+
       <StatusBar style="auto" />
     </View>
   );
