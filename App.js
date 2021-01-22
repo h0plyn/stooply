33564a4ac4f8ada53834b2cc17ae81c232ccb00d
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-22 12:02:04 -0500

    feat(request location permissions on app open)

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  const [error, setError] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setError('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
