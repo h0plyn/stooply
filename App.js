3943375bb9f89a1b8ad91ccaf9d34acfb923f878
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-22 11:23:17 -0500

    feat(implment a functional google map view)

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={StyleSheet.absoluteFillObject}
+        provider={MapView.PROVIDER_GOOGLE}
+      ></MapView>
       <StatusBar style="auto" />
     </View>
   );
