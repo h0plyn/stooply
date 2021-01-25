dde32f77644bea23013e0126198f38ac035457d4
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-25 12:00:44 -0500

    feat(style with styled components)

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,83 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import MapView, { Marker } from 'react-native-maps'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import { View, StyleSheet, Text, Button, ScrollView } from 'react-native'
+import styled from 'styled-components'
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`
+
+const LogoText = styled.Text`
+  font-size: 39;
+  color: black;
+`
+
+const ButtonView = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding: 25px;
+  justify-content: center;
+  align-items: center;
+`
+
+const MainView = styled.ScrollView`
+  display: flex;
+  border: black;
+  border-width: 5px
+  width: 80%;
+`
+const Footer = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 100px;
+`
 
 export default function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Stooply</Text>
-      <View>
-        <Button title="Browse Map" onPress={() => navigation.navigate('Map')} />
+    <Container>
+      <MainView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <LogoText>Stooply</LogoText>
+        <ButtonView>
+          <Button
+            title="Map"
+            color="black"
+            onPress={() => navigation.navigate('Map')}
+          />
+          <Button
+            color="black"
+            title="List"
+            onPress={() => navigation.navigate('List')}
+          />
+        </ButtonView>
+      </MainView>
+      <Footer>
         <Button
-          title="Browse List"
-          onPress={() => navigation.navigate('List')}
+          title="About"
+          color="black"
+          onPress={() => navigation.navigate('About')}
         />
-        <Button title="About" onPress={() => navigation.navigate('About')} />
         <Button
           title="Camera"
+          color="black"
           onPress={() => navigation.navigate('CameraScreen')}
         />
         <Button
           title="Add Item"
+          color="black"
           onPress={() => navigation.navigate('AddItem')}
         />
-      </View>
-    </View>
+      </Footer>
+    </Container>
   )
 }
 
