c30e2eaa3637a707a9967aca62cf5442a8148e7f
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-25 14:50:01 -0500

    feat(adjust styling to rounded borders for consistency)

--- screens/About.js
+++ screens/About.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import styled from 'styled-components'
 
 export default function About() {
   return (
-    <View>
-      <Text>
+    <Container>
+      <Title style={{ alignSelf: 'flex-start' }}>Origin Story</Title>
+      <FeatText>
         The concept for Stooply came about while walking around the Park Slope
-        neighborhood of Brooklyn in 2017
-      </Text>
-    </View>
+        neighborhood of Brooklyn in 2017.
+      </FeatText>
+    </Container>
   )
 }
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 10px;
+  margin-bottom: 150px;
+`
+
+const Title = styled.Text`
+  font-size: 39px;
+  color: black;
+`
+
+const FeatText = styled.Text`
+  font-size: 24px;
+  color: black;
+`
